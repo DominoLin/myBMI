e3617bdfc628a2ad5f992f32751259a6ffb9f558
DominoLin <x6760@outlook.com>
2019-11-03 16:26:43 +0800

    记录时间和BMI值

---
+++ app/src/main/java/com/example/mybmi/RecordBean.java
@@ -0,0 +1,22 @@
+package com.example.mybmi;
+
+public class RecordBean {
+    private String time;
+    private double result;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+}
